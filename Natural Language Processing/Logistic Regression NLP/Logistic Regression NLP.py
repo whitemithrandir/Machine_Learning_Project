0ea92eb1e995fd10e963aac0461f62f7e7dc7179
whitemithrandir <sabankara024@gmail.com>
2023-03-17 15:16:27 +0300

    v1.54

---
+++ Natural Language Processing/Logistic Regression NLP/Logistic Regression NLP.py
@@ -0,0 +1,283 @@
+# run this cell to import nltk
+import nltk
+from os import getcwd
+import re
+import string
+import numpy as np
+
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import TweetTokenizer
+
+nltk.download('twitter_samples')
+nltk.download('stopwords')
+
+import numpy as np
+import pandas as pd
+from nltk.corpus import twitter_samples 
+
+# select the set of positive and negative tweets
+all_positive_tweets = twitter_samples.strings('positive_tweets.json')
+all_negative_tweets = twitter_samples.strings('negative_tweets.json')
+
+# split the data into two pieces, one for training and one for testing (validation set) 
+test_pos = all_positive_tweets[4000:]
+train_pos = all_positive_tweets[:4000]
+test_neg = all_negative_tweets[4000:]
+train_neg = all_negative_tweets[:4000]
+
+train_x = train_pos + train_neg 
+test_x = test_pos + test_neg
+
+# combine positive and negative labels
+train_y = np.append(np.ones((len(train_pos), 1)), np.zeros((len(train_neg), 1)), axis=0)
+test_y = np.append(np.ones((len(test_pos), 1)), np.zeros((len(test_neg), 1)), axis=0)
+
+print("train_y.shape = " + str(train_y.shape))
+print("test_y.shape = " + str(test_y.shape))
+
+def process_tweet(tweet):
+    """Process tweet function.
+    Input:
+        tweet: a string containing a tweet
+    Output:
+        tweets_clean: a list of words containing the processed tweet
+
+    """
+    stemmer = PorterStemmer()
+    stopwords_english = stopwords.words('english')
+    # remove stock market tickers like $GE
+    tweet = re.sub(r'\$\w*', '', tweet)
+    # remove old style retweet text "RT"
+    tweet = re.sub(r'^RT[\s]+', '', tweet)
+    # remove hyperlinks    
+    tweet = re.sub(r'https?://[^\s\n\r]+', '', tweet)
+    # remove hashtags
+    # only removing the hash # sign from the word
+    tweet = re.sub(r'#', '', tweet)
+    # tokenize tweets
+    tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True,
+                               reduce_len=True)
+    tweet_tokens = tokenizer.tokenize(tweet)
+
+    tweets_clean = []
+    for word in tweet_tokens:
+        if (word not in stopwords_english and  # remove stopwords
+                word not in string.punctuation):  # remove punctuation
+            # tweets_clean.append(word)
+            stem_word = stemmer.stem(word)  # stemming word
+            tweets_clean.append(stem_word)
+
+    return tweets_clean
+
+def build_freqs(tweets, ys):
+    """Build frequencies.
+    Input:
+        tweets: a list of tweets
+        ys: an m x 1 array with the sentiment label of each tweet
+            (either 0 or 1)
+    Output:
+        freqs: a dictionary mapping each (word, sentiment) pair to its
+        frequency
+    """
+
+    """
+    Bu fonksiyon, bir tweet listesi ve duygu etiketi listesi (0 veya 1) alır ve (kelime, duygu)
+    çiftlerinin frekansını hesaplar. İşlemler aşağıdaki adımları içerir:
+
+    ys dizisini sıkıştırarak bir liste haline getirir.
+    Boş bir sözlük ile başlar ve tüm tweet'leri ve her tweet'teki işlenmiş kelimeleri
+    döngüyle dolaşarak sözlüğü doldurur.
+    Sözlük, (kelime, duygu) çiftini anahtar olarak alır ve frekansını değer olarak saklar.
+    Frekans sözlüğü sonunda döndürülür.
+    Bu fonksiyon, kelime ve duygu etiketlerinin frekansını hesaplayarak makine öğrenimi
+    modeli için bir veri kümesi oluşturmak için kullanılabilir.
+
+    """
+    # Convert np array to list since zip needs an iterable.
+    # The squeeze is necessary or the list ends up with one element.
+    # Also note that this is just a NOP if ys is already a list.
+    yslist = np.squeeze(ys).tolist()
+
+    # Start with an empty dictionary and populate it by looping over all tweets
+    # and over all processed words in each tweet.
+    freqs = {}
+    for y, tweet in zip(yslist, tweets):
+        for word in process_tweet(tweet):
+            pair = (word, y)
+            if pair in freqs:
+                freqs[pair] += 1
+            else:
+                freqs[pair] = 1
+
+    return freqs
+
+# create frequency dictionary
+freqs = build_freqs(train_x, train_y)
+
+# check the output
+print("type(freqs) = " + str(type(freqs)))
+print("len(freqs) = " + str(len(freqs.keys())))
+
+def sigmoid(z): 
+    '''
+    Input:
+        z: is the input (can be a scalar or an array)
+    Output:
+        h: the sigmoid of z
+    '''
+    
+    h = 1 / (1 + np.exp(-z))
+
+    return h
+
+def gradientDescent(x, y, theta, alpha, num_iters):
+    '''
+    Input:
+        x: matrix of features which is (m,n+1)
+        y: corresponding labels of the input matrix x, dimensions (m,1)
+        theta: weight vector of dimension (n+1,1)
+        alpha: learning rate
+        num_iters: number of iterations you want to train your model for
+    Output:
+        J: the final cost
+        theta: your final weight vector
+    Hint: you might want to print the cost to make sure that it is going down.
+    '''
+
+    # get 'm', the number of rows in matrix x
+    m = x.shape[0]
+    
+    for i in range(0, num_iters):
+        
+        # get z, the dot product of x and theta
+        z = np.dot(x,theta)
+        
+        # get the sigmoid of z
+        h = sigmoid(z)
+        
+        # calculate the cost function
+        J = -1./m * (np.dot(y.transpose(), np.log(h)) + np.dot((1-y).transpose(),np.log(1-h)))    
+
+        # update the weights theta
+        theta = theta = theta - (alpha/m) * np.dot(x.transpose(),(h-y))
+
+    J = float(J)
+    return J, theta
+
+def extract_features(tweet, freqs, process_tweet=process_tweet):
+    '''
+    Input: 
+        tweet: a list of words for one tweet
+        freqs: a dictionary corresponding to the frequencies of each tuple (word, label)
+    Output: 
+        x: a feature vector of dimension (1,3)
+    '''
+    # process_tweet tokenizes, stems, and removes stopwords
+    word_l = process_tweet(tweet)
+    
+    # 3 elements in the form of a 1 x 3 vector
+    x = np.zeros((1, 3)) 
+    
+    #bias term is set to 1
+    x[0,0] = 1 
+    
+    
+    # loop through each word in the list of words
+    for word in word_l:
+        
+        # increment the word count for the positive label 1
+        x[0,1] += freqs.get((word, 1.0),0)
+        
+        # increment the word count for the negative label 0
+        x[0,2] += freqs.get((word, 0.0),0)
+    assert(x.shape == (1, 3))
+    return x
+
+
+# collect the features 'x' and stack them into a matrix 'X'
+X = np.zeros((len(train_x), 3))
+for i in range(len(train_x)):
+    X[i, :]= extract_features(train_x[i], freqs)
+
+# training labels corresponding to X
+Y = train_y
+
+# Apply gradient descent
+J, theta = gradientDescent(X, Y, np.zeros((3, 1)), 1e-9, 1500)
+print(f"The cost after training is {J:.8f}.")
+print(f"The resulting vector of weights is {[round(t, 8) for t in np.squeeze(theta)]}")
+
+def predict_tweet(tweet, freqs, theta):
+    '''
+    Input: 
+        tweet: a string
+        freqs: a dictionary corresponding to the frequencies of each tuple (word, label)
+        theta: (3,1) vector of weights
+    Output: 
+        y_pred: the probability of a tweet being positive or negative
+    '''
+   
+    # extract the features of the tweet and store it into x
+    x = extract_features(tweet,freqs)
+    
+    # make the prediction using x and theta
+    y_pred = sigmoid(np.dot(x,theta))
+  
+    return y_pred
+
+
+def test_logistic_regression(test_x, test_y, freqs, theta, predict_tweet=predict_tweet):
+    """
+    Input: 
+        test_x: a list of tweets
+        test_y: (m, 1) vector with the corresponding labels for the list of tweets
+        freqs: a dictionary with the frequency of each pair (or tuple)
+        theta: weight vector of dimension (3, 1)
+    Output: 
+        accuracy: (# of tweets classified correctly) / (total # of tweets)
+    """
+
+    # the list for storing predictions
+    y_hat = []
+    
+    for tweet in test_x:
+        # get the label prediction for the tweet
+        y_pred = predict_tweet(tweet, freqs, theta)
+        
+        if y_pred > 0.5:
+            # append 1.0 to the list
+            y_hat.append(1)
+        else:
+            # append 0 to the list
+            y_hat.append(0)
+
+    # With the above implementation, y_hat is a list, but test_y is (m,1) array
+    # convert both to one-dimensional arrays in order to compare them using the '==' operator
+    accuracy = (y_hat==np.squeeze(test_y)).sum()/len(test_x)
+    
+    return accuracy
+
+tmp_accuracy = test_logistic_regression(test_x, test_y, freqs, theta)
+print(f"Logistic regression model's accuracy = {tmp_accuracy:.4f}")
+
+# Some error analysis done for you
+print('Label Predicted Tweet')
+for x,y in zip(test_x,test_y):
+    y_hat = predict_tweet(x, freqs, theta)
+
+    if np.abs(y - (y_hat > 0.5)) > 0:
+        print('THE TWEET IS:', x)
+        print('THE PROCESSED TWEET IS:', process_tweet(x))
+        print('%d\t%0.8f\t%s' % (y, y_hat, ' '.join(process_tweet(x)).encode('ascii', 'ignore')))
+
+
+# Feel free to change the tweet below
+my_tweet = 'This is a ridiculously bright movie. The plot was terrible and I was sad until the ending!'
+print(process_tweet(my_tweet))
+y_hat = predict_tweet(my_tweet, freqs, theta)
+print(y_hat)
+if y_hat > 0.5:
+    print('Positive sentiment')
+else: 
+    print('Negative sentiment')
